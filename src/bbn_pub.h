17eafc068a054af7fdc2eb23b32eb19ba5a9734c
ghostxmerlin <ghostxmerlin@gmail.com>
2025-08-01 15:04:34 +0800

    add pub handler

---
+++ src/bbn_pub.h
@@ -0,0 +1,4 @@
+
+bool bbn_derive_staker_pubkey_from_policy(sign_psbt_state_t *st,
+                                          uint32_t input_index,
+                                          uint8_t *staker_pk_out);
